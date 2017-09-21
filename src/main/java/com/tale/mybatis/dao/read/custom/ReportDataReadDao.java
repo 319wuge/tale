9bebfc87e9505c7ed1c0f49e7c48e4cb2d1d3d91
tonggen <1799735174@qq.com>
2017-09-21 16:58:02 +0800

    加入mybatis

---
+++ src/main/java/com/tale/mybatis/dao/read/custom/ReportDataReadDao.java
@@ -0,0 +1,6 @@
+package com.tale.mybatis.dao.read.custom;
+
+import com.tale.mybatis.dao.read.mbg.MBGReportDataReadDao;
+
+public interface ReportDataReadDao extends MBGReportDataReadDao {
+}
---
+++ src/main/java/com/tale/mybatis/dao/write/custom/ReportDataWriteDao.java
@@ -0,0 +1,6 @@
+package com.tale.mybatis.dao.write.custom;
+
+import com.tale.mybatis.dao.write.mbg.MBGReportDataWriteDao;
+
+public interface ReportDataWriteDao extends MBGReportDataWriteDao {
+}
